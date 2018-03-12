9efeacc5b1c1ce00e953472a4de5c1b4f8468236
Michelle <mw.michiko.85@gmail.com>
2018-03-12 19:34:22 +0000

    has some color and styling now but still has layout and responsive issues

--- src/js/views/Dashboard.jsx
+++ src/js/views/Dashboard.jsx
@@ -13,71 +13,137 @@ export default class Dashboard extends Flux.View {
         
         return(
             <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between navbar-expand-sm">
-                <a className="navbar-brand" href="#">Our Meetup</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item active">
-                            <Link className="nav-link nav-item" to="/"> Dashboard</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+              {/*this is the nav and logo bar */}
+                <nav className="navbar navbar-expand-lg navbar-light justify-content-between navbar-expand-sm">
+                    <a className="navbar-brand" href="#">grouped</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item active">
+                                <Link className="nav-link nav-item theDashboard" to="/"> Dashboard</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
             <div className="jumbotron jumbotron-fluid">
                 <div className="container text-center">
-                    <h1 className="display-4">Fluid jumbotron</h1>
-                    <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                    <h1 className="display-4 jumboTitle">Let's Get Together Tonight</h1>
+                    <p className="lead jumboSubTitle">a place to meet new friends and maintain current friendships</p>
                 </div>
             </div>
+            
             <div className="container">
                 <div className="row">
-                    <div className="col-8">
+                
+                 {/* event 1 */}
+                    <div className="col-md-8">
                         <div className="row">
                             <div className="col-12">
-                            <div className="card">
-                            <div className="card-header">
-                                <h2>Monday, October 23</h2>
-                            </div>
-                            <div className="card-body">
-                                <div className="row">
-                                    <div className="col-2">
-                                        <h5>11:00AM</h5>
+                                <div className="card">
+                                    <div className="card-header">
+                                        <h2>Tuesday, March 13</h2>
                                     </div>
-                                    <div className="col-10">
-                                        <h5 className="card-title">Event Title</h5>
-                                        <p className="card-text">Event details provided here</p>
-                                        <a href="#" className="btn btn-primary">See Details</a>
+                                    <div className="card-body">
+                                        <div className="row">
+                                            <div className="col-md-2">
+                                                <h5>6:00PM</h5>
+                                            </div>
+                                            <div className="col-md-10">
+                                                <h5 className="card-title">Cats + Coffee</h5>
+                                                <p className="card-text">We supply the cats, the coffee, and the cozy chairs.</p>
+                                                <a href="#" className="btn btn-primary">See Details</a>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                    
+                     {/* event 2 */}
+                    <div className="col-md-8">
+                        <div className="row">
+                            <div className="col-12">
+                                <div className="card">
+                                    <div className="card-header">
+                                        <h2>Friday, March 30</h2>
+                                    </div>
+                                    <div className="card-body">
+                                        <div className="row">
+                                            <div className="col-md-2">
+                                                <h5>7:00PM</h5>
+                                            </div>
+                                            <div className="col-md-10">
+                                                <h5 className="card-title">Let's Relax Together</h5>
+                                                <p className="card-text">Bring a bottle of wine and meets us at Party Platter for a good time.</p>
+                                                <a href="#" className="btn btn-primary">See Details</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     
                     
+                     {/* event 3 */}
+                    <div className="col-md-8">
+                        <div className="row">
+                            <div className="col-12">
+                                <div className="card">
+                                    <div className="card-header">
+                                        <h2>Sunday, April 1</h2>
+                                    </div>
+                                    <div className="card-body">
+                                        <div className="row">
+                                            <div className="col-md-2">
+                                                <h5>10:00AM</h5>
+                                            </div>
+                                            <div className="col-md-10">
+                                                <h5 className="card-title">Hiking with Harry</h5>
+                                                <p className="card-text">Harry takes you through the woods and back again safely. Your kids are invited!</p>
+                                                <a href="#" className="btn btn-primary">See Details</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     
-                    
-                    
-                    
-                        <div className="col-4">
+                    {/* the calendar */}
+                        <div className="col-md-4">
                             <InfiniteCalendar
                                 width={400}
                                 height={600}
                                 selected={today}
                                 disabledDays={[0,6]}
                                 minDate={lastWeek}
-                        />
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
         
-        
-        
-        
+                {/* the footer */}
+                <footer className="footer" id="footer">
+	                <div className="container">
+	                    <div className="row justify-content-between">
+			                <div className="col-6">
+				                <img className="img-fluid" src="http://placehold.it/50x50" alt="" />
+			                </div>
+                			<div className="col-6">
+                				<ul className="footer__links list-inline text-right">
+                					<li className="footer__link list-inline-item">Blog</li>
+                					<li className="footer__link list-inline-item">Contact Us</li>
+                				</ul>
+                			</div>
+			
+		                    </div>
+	                   </div>
+                    </footer>
+            
    
             </div>
 
--- src/js/views/Events.jsx
+++ src/js/views/Events.jsx
@@ -10,7 +10,7 @@ export default class Events extends Flux.View {
             <div>
                 {/*this is the nav and logo bar */}
                    <nav className="navbar navbar-expand-lg navbar-light justify-content-between navbar-expand-sm">
-                    <a className="navbar-brand" href="#">Group up</a>
+                    <a className="navbar-brand" href="#">grouped</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -27,23 +27,24 @@ export default class Events extends Flux.View {
                 {/*this is the jumbotron info bar */}
                     <div className="jumbotron jumbotron-fluid eventHero">
                         <div className="container-fluid">
-                        
-                        {/*left side */}
                             <div className="row">
+                        {/*left side */}
                                 <div className="col-md-8 jumboLeft">
                                     <div className="row">
                                         <div className="col-12">
                                             <p className="eventDate">Tuesday, March 13, 2018</p>
                                             <h1 className="eventTitle"> Cats + Coffee All Day</h1>
                                             <div className="row">
-                                               <div className="col-md-1">
-                                                    <img src="//placehold.it/50" className    ="rounded-circle" />
+                                                <div>
+                                                    <img src="//placehold.it/50" className="rounded-circle" />
                                                 </div>
-                                                <div className="col-md-11 theHost">
+                                                <div className="authors">
                                                     <span className="text">Hosted by</span>
                                                     <span className="link"><Link to="/"> Mr. Whiskers</Link></span>
                                                     <p><span className="text">From</span>
-                                                    <span className="link"><Link to="/"> Pur-Town    </Link></span></p>
+                                                    <span className="link"><Link to="/"> Pur-Town</Link></span></p>
+                                                </div>        
+                                
                                                  </div>  
                                             </div>
                                         </div>
@@ -55,15 +56,15 @@ export default class Events extends Flux.View {
                                     <div className="attendance">
                                         <p><strong>Are you going?</strong> X people going</p>
                                     </div>
-                                    <div className="row rsvpBTN">
-                                        <div className="col-md-6">
-                                            <button type="button" className="btn btn-primary w-100">Yes</button>
+                                    <div className="row rsvpBTN flex-nowrap">
+                                        <div className="col-md-5">
+                                            <button type="button" className="btn btn-primary w-100 yesBTN">Yes</button>
                                         </div>
-                                        <div className="col-md-6">
-                                            <button type="button" className="btn btn-primary w-100">No</button>
+                                        <div className="col-md-5">
+                                            <button type="button" className="btn btn-primary w-100 noBTN">No</button>
                                         </div>
                                     </div>
-                                    <div className="row socialMedia">
+                                    <div className="row socialMedia flex-nowrap">
                                         <div className="col-md-6">
                                             <button type="button" className="btn-floating btn-sm btn-twi"><i className="fab fa-twitter"></i></button>
                                             <span>Tweet It</span>
@@ -76,35 +77,69 @@ export default class Events extends Flux.View {
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    
                     
                 {/*body of page */}
                 <div className="container-fluid">
                     <div className="row">
                         {/*body left side*/}
-                        <div className="col-7">
+                        <div className="col-md-7">
                             <div className="row">
                                 <div className="col-12">
-                                <img className="eventPhoto" src="http://via.placeholder.com/500X300" alt="event image of..." />
-                                <h5><strong>Details</strong></h5>
-                                <p>Example body text describing the event. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget enim nec enim pharetra ultrices. Maecenas placerat interdum lectus, non dictum felis vehicula at. </p>
-                                <p>Sed rutrum venenatis dolor, sit amet bibendum mi consectetur at. In hac habitasse platea dictumst. Nullam facilisis dignissim nisi eget cursus. Vestibulum sed libero mauris. Nam elementum lobortis purus, vitae rhoncus justo mattis non. Morbi id erat at nunc finibus dignissim. </p>
-                    
+                                <img className="img-fluid eventPhoto" src="http://via.placeholder.com/500X300" alt="event image of..." />
+                                <h5 className="details"><strong>Details</strong></h5>
+                                <p className="bodyText">Snuggle up with cute kitties, hot lattes, and a book. We host this event twice a month for a place to socialize or maybe come out for alternative therapy.<br/><br/>There are a few rules to follow for this event:</p>
+                                <ul>
+                                    <li>You must purchase a beverage (e.g. coffee, tea, cocoa, etc)</li>
+                                    <li>Stay as long as you like but only 30 minutes with each cat</li>
+                                    <li>Be kind to cats and humans alike</li>
+                                    <li>No children under 12 years old. This event is an escape for most people who attend</li>
+                                    <li>Must wear headphones for phone calls and music</li>
+                                </ul>
                                 </div>
                             </div>
                         </div>
                         
                          {/*body right side*/}
-                        <div className="col-5">
+                        <div className="col-md-5 order-first">
                             <div className="card">
-                                <img top ="w-100" src="http://via.placeholder.com/350x150" alt="Card image cap" />                            
-                            <div className="card-body">
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+                                <div className="card-body">
+                                    <div className="row cardInfo">
+                                        <div>
+                                            <span> <i className="far fa-clock"></i></span>
+                                        </div>
+                                        <div>
+                                            <span className="card-date">Tuesday, March 13, 2018</span><br/>
+                                            <span className="card-time">6:00 PM to 9:00 PM</span><br/>
+                                            <span className="card-schedule">Every first and last Tuesday of the month</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <img top ="w-100" src="http://via.placeholder.com/350x150" alt="map of the location" />                            
+                                
                             </div>
                         </div>
                     </div>
                 </div>
+                
+                {/* the footer */}
+                <footer className="footer" id="footer">
+	                <div className="container">
+	                    <div className="row justify-content-between">
+			                <div className="col-6">
+				                <img className="img-fluid" src="http://placehold.it/50x50" alt="" />
+			                </div>
+                			<div className="col-6">
+                				<ul className="footer__links list-inline text-right">
+                					<li className="footer__link list-inline-item">Blog</li>
+                					<li className="footer__link list-inline-item">Contact Us</li>
+                				</ul>
+                			</div>
+			
+		                    </div>
+	                   </div>
+                    </footer>
+                
             </div>
 
             );
